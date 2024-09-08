ce56cb15d19307212158fde32b69a0d3680eebf1
spindart <spindart@proton.me>
2024-09-08 18:44:25 -0300

    feat: database connection

--- app/Infrastructure/Persistence/ConnectionCreator.php
+++ app/Infrastructure/Persistence/ConnectionCreator.php
@@ -9,8 +9,23 @@ class ConnectionCreator
     public static function getConnection(): \PDO
     {
         if (is_null(self::$pdo)) {
-            $pathDatabase = __DIR__ . '/../../database.sqlite';
-            self::$pdo = new \PDO('sqlite:' . $pathDatabase);
+            // Carregar configurações
+            $config = require __DIR__ . '/../../../config/database.php';
+
+            $driver = $config['db']['driver'];
+
+            if ($driver === 'sqlite') {
+                $pathDatabase = $config['db']['sqlite']['database'];
+                self::$pdo = new \PDO('sqlite:' . $pathDatabase);
+            } elseif ($driver === 'mysql') {
+                $host = $config['db']['mysql']['host'];
+                $dbname = $config['db']['mysql']['dbname'];
+                $user = $config['db']['mysql']['user'];
+                $password = $config['db']['mysql']['password'];
+                $charset = $config['db']['mysql']['charset'];
+                $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+                self::$pdo = new \PDO($dsn, $user, $password);
+            }
             self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
 
